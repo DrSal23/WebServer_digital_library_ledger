fb2cfbedec6ee3b24faa41aee80ba4d7e7dd1061
Salah Mahmoud <smah1219@students.vsu.edu>
2024-12-09 00:15:25 -0500

    change start dir to index and update files

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const connectDB = require('./models/db');
+const bookRoutes = require('./routes/bookRoutes');
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+// Middleware to parse incoming JSON
+app.use(bodyParser.json());
+
+// Routes
+app.use('/books', bookRoutes);
+
+module.exports = app;
+ 
